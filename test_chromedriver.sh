ab2f569793acebf342e2ceb69a75426604b1f476
kirkcoper <143509963+kirkcoper@users.noreply.github.com>
2023-08-29 23:35:37 +0800

    Update test_chromedriver.sh

--- test_chromedriver.sh
+++ test_chromedriver.sh
@@ -16,9 +16,12 @@ ARCH=$2
 
 if [[ "${ARCH}" =~ ^linux64 ]]; then
     CHROMEAPP=google-chrome
-    echo ""
+    echo "默认已经安装过了 谷歌浏览器"
     echo "CHROME VERSION " "${CHROMEAPP}" --version  $("${CHROMEAPP}" --version )
     echo " "
+    cat /etc/apt/sources.list.d/google.list
+    echo " "
+    # 超级管理员
     sudo=$(command -v sudo)
     APP="${CHROMEAPP}"
     if ! dpkg -s "${APP}" >/dev/null; then
@@ -71,6 +74,53 @@ echo ""
 echo "ARCH ${ARCH}"
 echo ""
 
+echo "Installing chromedriver ..."
+echo ""
+if ((CHROME_VERSION < 115)); then
+    if [[ -z "${VERSION}" ]]; then
+        URL="https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION}"
+        echo "Downloading ${URL}..."
+        VERSION=$(${CURL} "${URL}")
+        echo "VERSION=${VERSION}"
+    fi
+    echo "Installing ChromeDriver ${VERSION} for ${ARCH}"
+    URL="https://chromedriver.storage.googleapis.com/${VERSION}/chromedriver_${ARCH}.zip"
+    echo "Downloading ${URL}..."
+    ${CURL} -o chromedriver.zip "${URL}"
+    unzip -o -q chromedriver.zip
+    sudo mv chromedriver /usr/local/bin/chromedriver
+    rm -f chromedriver.zip
+    exit
+fi
+
+if [[ -z "${VERSION}" ]]; then
+    VERSION=$("${CHROMEAPP}" --version | cut -f 3 -d ' ')
+    echo "VERSION=${VERSION}"
+fi
+if [[ "${ARCH}" =~ ^mac64 ]]; then
+    ARCH="mac-x64"
+fi
+
+echo "Downloading ${JSON_URL}..."
+JSON=$(${CURL} "${JSON_URL}")
+JQ=".versions[] | select(.version == \"${VERSION}\") | .downloads.chromedriver[] | select(.platform == \"${ARCH}\") | .url"
+URL=$(jq -r "${JQ}" <<<"${JSON}")
+if [[ -z "${URL}" ]]; then
+    echo "Falling back to latest version of ChromeDriver for ${ARCH}"
+    VERSION3=$(cut -d '.' -f1-3 <<<"${VERSION}")
+    echo "VERSION3=${VERSION3}"
+    JQ2="[ .versions[] | select(.version | startswith(\"${VERSION3}.\")) ] | last | .version"
+    VERSION=$(jq -r "${JQ2}" <<<"${JSON}")
+    echo "VERSION=${VERSION}"
+    JQ3=".versions[] | select(.version == \"${VERSION}\") | .downloads.chromedriver[] | select(.platform == \"${ARCH}\") | .url"
+    URL=$(jq -r "${JQ3}" <<<"${JSON}")
+fi
+echo "Installing ChromeDriver ${VERSION} for ${ARCH}"
+echo "Downloading ${URL}..."
+${CURL} -o chromedriver.zip "${URL}"
+unzip -o -q chromedriver.zip
+sudo mv "chromedriver-${ARCH}/chromedriver" /usr/local/bin/chromedriver
+rm -fr chromedriver.zip chromedriver-*
 
 
 
